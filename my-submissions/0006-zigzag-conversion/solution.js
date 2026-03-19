1e058b69412ab09fe0d7e6ca7e7d897c23dcb358
Amrit Singh <amrit.sbagga@gmail.com>
2026-03-19 12:25:58 +0000

    Sync LeetCode submission Runtime - 2 ms (97.20%), Memory - 57.4 MB (83.27%)

---
+++ my-submissions/0006-zigzag-conversion/solution.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function (s, numRows) {
+    if (!s || s.length === 0) {
+        return '';
+    }
+
+    if (numRows === 1) {
+        return s;
+    }
+
+    let result = '';
+    let step = 2 * numRows - 2;
+
+    for (let i = 0; i < numRows; i++) {
+        for (let j = i; j < s.length; j += step) {
+            result += s[j];
+
+            if (i != 0 && i != numRows - 1 && (j + step - 2 * i) < s.length) {
+                result += s[j + step - 2 * i];
+            }
+        }
+    }
+
+    return result;
+
+};
