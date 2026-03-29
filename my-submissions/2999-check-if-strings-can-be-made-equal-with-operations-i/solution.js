1649eb3de3d69f532e7c1072bf3e106067af5dd1
Amrit Singh <amrit.sbagga@gmail.com>
2026-03-29 11:58:13 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 56.2 MB (66.89%)

---
+++ my-submissions/2999-check-if-strings-can-be-made-equal-with-operations-i/solution.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var canBeEqual = function (s1, s2) {
+    // (0, 2) & (1, 3) position compare
+    return ((s1[0] === s2[0] && s1[2] === s2[2]) ||
+        (s1[0] === s2[2] && s1[2] === s2[0])) &&
+        ((s1[1] === s2[1] && s1[3] === s2[3]) ||
+            (s1[1] === s2[3] && s1[3] === s2[1]));
+};
