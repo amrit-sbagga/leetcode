a8a72211429ff9fe24ac6dae3366c7b8383cf1f4
Amrit Singh <amrit.sbagga@gmail.com>
2026-04-02 06:56:42 +0000

    Sync LeetCode submission Runtime - 5 ms (22.09%), Memory - 64.6 MB (91.45%)

---
+++ my-submissions/0121-best-time-to-buy-and-sell-stock/solution.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+    let left = 0; // Buy
+    let right = 1; // Sell
+    let max_profit = 0;
+    while (right < prices.length) {
+        if (prices[left] < prices[right]) {
+            let profit = prices[right] - prices[left];
+            max_profit = Math.max(max_profit, profit);
+        } else {
+            left = right;
+        }
+        right++;
+    }
+    return max_profit;
+}
