6db8cc2ec6d56c48902f64567700a4be980d32f6
Amrit Singh <amrit.sbagga@gmail.com>
2026-04-19 14:21:09 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 55.3 MB (55.00%)

---
+++ my-submissions/4168-mirror-distance-of-an-integer/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var mirrorDistance = function(n) {
+
+    const getReverse = () => {
+        let rev = 0;
+        while(n > 0){
+            rev = rev * 10 + n % 10;
+            n = Math.floor(n/10);
+        }
+        return rev;
+    }
+    // console.log(Math.abs(n - getReverse(n)));
+    return Math.abs(n - getReverse(n));
+};
