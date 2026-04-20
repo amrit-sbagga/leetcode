df1576c7cb182f220a6cf36c8e7ce4b9b5b1d318
Amrit Singh <amrit.sbagga@gmail.com>
2026-04-20 07:56:29 +0000

    Sync LeetCode submission Runtime - 47 ms (29.05%), Memory - 53.9 MB (32.61%)

--- my-submissions/2732-counter/solution.js
+++ my-submissions/2732-counter/solution.js
@@ -3,14 +3,15 @@
  * @return {Function} counter
  */
 var createCounter = function(n) {
+    let count = n;
     return function() {
-        return n++;
+        return count++;
     };
 };
 
-/** 
- * const counter = createCounter(10)
- * counter() // 10
- * counter() // 11
- * counter() // 12
- */
+
+ const counter = createCounter(10)
+ counter() // 10
+ counter() // 11
+ counter() // 12
+ 
