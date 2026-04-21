5c43f856747b109627867230f40e0046be352198
Amrit Singh <amrit.sbagga@gmail.com>
2026-04-21 14:23:38 +0000

    Sync LeetCode submission Runtime - 700 ms (22.43%), Memory - 0.0B (100.00%)

--- my-submissions/1811-fix-names-in-a-table/solution.sql
+++ my-submissions/1811-fix-names-in-a-table/solution.sql
@@ -1,4 +1,4 @@
--- Write your PostgreSQL query statement below
+# Write your MySQL query statement below
 Select user_id, 
    CONCAT(UPPER(SUBSTRING(name, 1, 1)), LOWER(SUBSTRING(NAME, 2))) as name
 FROM Users
