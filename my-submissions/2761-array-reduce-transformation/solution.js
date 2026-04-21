419157e8041af056f68fd8267013ebccfbe7755f
Amrit Singh <amrit.sbagga@gmail.com>
2026-04-21 07:19:24 +0000

    Sync LeetCode submission Runtime - 53 ms (16.36%), Memory - 57.1 MB (5.04%)

--- my-submissions/2761-array-reduce-transformation/solution.js
+++ my-submissions/2761-array-reduce-transformation/solution.js
@@ -4,10 +4,10 @@
  * @param {number} init
  * @return {number}
  */
-var reduce = function (nums, fn, init) {
+var reduce = function(nums, fn, init){
     let val = init;
-    for (const num of nums) {
-        val = fn(val, num);
+    for(const num of nums){
+       val = fn(val, num);
     }
     return val;
-};
+}
