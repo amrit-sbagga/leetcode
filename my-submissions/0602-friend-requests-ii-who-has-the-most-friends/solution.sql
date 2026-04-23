fefcd84c0bf1c0fffa04af0a31e8964eb927dc61
Amrit Singh <amrit.sbagga@gmail.com>
2026-04-23 15:55:44 +0000

    Sync LeetCode submission Runtime - 323 ms (27.88%), Memory - 0.0B (100.00%)

---
+++ my-submissions/0602-friend-requests-ii-who-has-the-most-friends/solution.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+with cte as (
+    select requester_id as id from RequestAccepted
+    UNION ALL
+    select accepter_id as id from RequestAccepted
+)
+select id, count(*) as num from cte group by id order by num desc limit 1
