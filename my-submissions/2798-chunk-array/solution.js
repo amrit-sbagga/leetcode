7041246b48139b2bdab24246e3b87865edae7b97
Amrit Singh <amrit.sbagga@gmail.com>
2026-04-25 15:39:18 +0000

    Sync LeetCode submission Runtime - 55 ms (14.95%), Memory - 59.3 MB (5.52%)

--- my-submissions/2798-chunk-array/solution.js
+++ my-submissions/2798-chunk-array/solution.js
@@ -3,12 +3,22 @@
  * @param {number} size
  * @return {Array}
  */
-var chunk = function (arr, size) {
-    const chunkedArray = [];
-    for (let i = 0; i < arr.length; i += size) {
-        chunkedArray.push(arr.slice(i, i + size));
-    }
-    console.log("Output: ", { chunkedArray })
-    return chunkedArray;
-};
+// var chunk = function (arr, size) {
+//     const chunkedArray = [];
+//     for (let i = 0; i < arr.length; i += size) {
+//         chunkedArray.push(arr.slice(i, i + size));
+//     }
+//     console.log("Output: ", { chunkedArray })
+//     return chunkedArray;
+// };
 
+
+var chunk = (arr, size) => {
+   const chunkedArr = [];
+   for(let i = 0; i < arr.length; i += size) {
+       chunkedArr.push(arr.slice(i, i + size));
+   }
+
+   console.log("Output : ", { chunkedArr })
+   return chunkedArr;
+}
