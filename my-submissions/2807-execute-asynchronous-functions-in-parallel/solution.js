deccc373d37ef20e6dae52d12b665c81c337775e
Amrit Singh <amrit.sbagga@gmail.com>
2026-04-27 12:54:23 +0000

    Sync LeetCode submission Runtime - 66 ms (39.02%), Memory - 54.8 MB (38.13%)

---
+++ my-submissions/2807-execute-asynchronous-functions-in-parallel/solution.js
@@ -0,0 +1,27 @@
+/**
+ * @param {Array<Function>} functions
+ * @return {Promise<any>}
+ */
+var promiseAll = function(functions) {
+    return new Promise((resolve, reject) => {
+       const results = [];
+       let completed = 0;
+
+       functions.forEach((fn, index) => {
+            fn().then((result) => {
+              results[index] = result;
+              completed += 1;
+              if(completed === functions.length){
+                resolve(results);
+              }
+            }).catch((error) => {
+               reject(error);
+            });
+       });
+    });
+};
+
+/**
+ * const promise = promiseAll([() => new Promise(res => res(42))])
+ * promise.then(console.log); // [42]
+ */
