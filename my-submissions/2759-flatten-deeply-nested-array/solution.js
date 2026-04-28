93d180b3ce91476d54b11e929eea5e16e9b4f2b4
Amrit Singh <amrit.sbagga@gmail.com>
2026-04-28 13:09:27 +0000

    Sync LeetCode submission Runtime - 169 ms (12.31%), Memory - 94.1 MB (7.93%)

--- my-submissions/2759-flatten-deeply-nested-array/solution.js
+++ my-submissions/2759-flatten-deeply-nested-array/solution.js
@@ -3,14 +3,26 @@
  * @param {number} depth
  * @return {Array}
  */
-var flat = function (arr, n) {
-    // const stack = [...arr.map(item => [item, n])];
-    // console.log(stack);
+// var flat = function (arr, n) {
+//     // const stack = [...arr.map(item => [item, n])];
+//     // console.log(stack);
+//     const result = [];
+//     for (var x of arr) {
+//         if (Array.isArray(x) && n > 0) result.push(...flat(x, n - 1))
+//         else result.push(x);
+//     }
+//     return result;
+// };
 
+var flat = function (arr, n) {
     const result = [];
-    for (var x of arr) {
-        if (Array.isArray(x) && n > 0) result.push(...flat(x, n - 1))
-        else result.push(x);
+
+    for(var x of arr) {
+       if(Array.isArray(x) && n > 0) {
+          result.push(...flat(x, n - 1))
+       } else {
+          result.push(x)
+       }
     }
     return result;
-};
+}
