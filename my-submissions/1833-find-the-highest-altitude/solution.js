90680fd561d0cd8ef93ba3adc0e84ddbd28db60f
Amrit Singh <amrit.sbagga@gmail.com>
2026-05-11 08:03:12 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 53.7 MB (46.64%)

---
+++ my-submissions/1833-find-the-highest-altitude/solution.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} gain
+ * @return {number}
+ */
+var largestAltitude = function(gain) {
+    let previous = 0;
+    let biggest = 0;
+
+    for(let i = 0; i < gain.length; i++){
+        previous += gain[i];
+        if(previous > biggest) biggest = previous;
+    }
+    return biggest;
+}
