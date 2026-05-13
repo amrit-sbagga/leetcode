d648936883f5719aba0bac2f27ab45790c9c6748
Amrit Singh <amrit.sbagga@gmail.com>
2026-05-13 10:37:27 +0000

    Sync LeetCode submission Runtime - 3 ms (39.96%), Memory - 55.6 MB (65.14%)

--- my-submissions/0151-reverse-words-in-a-string/solution.js
+++ my-submissions/0151-reverse-words-in-a-string/solution.js
@@ -3,7 +3,18 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-    s = s.trim();
-    let splitArr = s.split(/\s+/);
-    return splitArr.reverse().join(' ');
+    // s = s.trim();
+    // let splitArr = s.split(/\s+/);
+    // return splitArr.reverse().join(' ');
+
+    let resArr = [];
+    let splitArr = s.trim().split(" ");
+    for(let i = splitArr.length - 1; i >= 0; i--){
+        let word = splitArr[i];
+        if(word){
+            resArr.push(word + " ") 
+        }
+    }
+    // console.log("resArr =>", resArr.join(""));
+    return resArr.join("").trim();
 };
