af90f281b771c0fdbfa7350b8607516840735b1d
Amrit Singh <amrit.sbagga@gmail.com>
2026-05-14 20:24:05 +0000

    Sync LeetCode submission Runtime - 2 ms (100.00%), Memory - 100 MB (58.09%)

---
+++ my-submissions/2216-delete-the-middle-node-of-a-linked-list/solution.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteMiddle = function (head) {
+    if (head === null || head.next == null) {
+        return null;
+    }
+
+    let slow = head;
+    let fast = head.next;
+    while (fast.next != null && fast.next.next != null) {
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+    //delete
+    slow.next = slow.next.next;
+    return head;
+};
