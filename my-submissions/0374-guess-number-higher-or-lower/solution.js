adb6127335e54a20c1310b6c182f036885250d8e
Amrit Singh <amrit.sbagga@gmail.com>
2026-05-15 10:10:25 +0000

    Sync LeetCode submission Runtime - 44 ms (45.61%), Memory - 53.6 MB (54.97%)

---
+++ my-submissions/0374-guess-number-higher-or-lower/solution.js
@@ -0,0 +1,33 @@
+/** 
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * var guess = function(num) {}
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var guessNumber = function (n) {
+
+    let start = 1;
+    let end = n;
+    while (start <= end) {
+        // let mid = Math.floor((start + end) / 2);
+        let mid = start + Math.floor((end - start) / 2);
+
+        const result = guess(mid);
+        if (result === 0) {
+            return mid;
+        } else if (result > 0) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+
+    return -1;
+};
