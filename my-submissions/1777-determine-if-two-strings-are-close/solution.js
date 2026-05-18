65917e9d83d989c0a2e327380147f0f62c848b08
Amrit Singh <amrit.sbagga@gmail.com>
2026-05-18 12:08:37 +0000

    Sync LeetCode submission Runtime - 92 ms (20.51%), Memory - 65.8 MB (17.72%)

---
+++ my-submissions/1777-determine-if-two-strings-are-close/solution.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {boolean}
+ */
+
+const getSortedItems = (word) => {
+    const group = {};
+
+    for (let c of word) {
+        group[c] = (group[c] || 0) + 1;
+    }
+
+    console.log("word, group =>", word, group);
+
+    return {
+        keys: Object.keys(group).sort(),
+        counts: Object.values(group).sort((a, b) => a - b)
+    }
+}
+
+var closeStrings = function (word1, word2) {
+    if (word1.length != word2.length) return false;
+
+
+    const group1 = getSortedItems(word1);
+    const group2 = getSortedItems(word2);
+
+    for (let i = 0; i < group1.keys.length; i++) {
+        if (group1.keys[i] !== group2.keys[i] || group1.counts[i] !== group2.counts[i]) {
+            return false;
+        }
+    }
+    return true;
+};
