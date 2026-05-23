bb6d6063d5590007fda0ab3076e4bb6c2b5e02a3
Amrit Singh <amrit.sbagga@gmail.com>
2026-05-23 08:58:47 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.1 MB (44.91%)

---
+++ my-submissions/1878-check-if-array-is-sorted-and-rotated/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean check(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+
+        // count no of positions where prev element is greater than current
+        for(int i = 1; i < n; i++) {
+            if(nums[i - 1] > nums[i]) {
+              count++;
+            }
+        }
+
+        // circular break check between last element & first element
+        if(nums[n - 1] > nums[0]){
+          count++;
+        }
+        
+        return count <= 1;
+        
+    }
+}
