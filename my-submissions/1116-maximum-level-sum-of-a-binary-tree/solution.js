0030547ddd24d890d873bf41edb321956e0a5702
Amrit Singh <amrit.sbagga@gmail.com>
2026-05-29 12:13:45 +0000

    Sync LeetCode submission Runtime - 54 ms (16.13%), Memory - 74.9 MB (44.76%)

---
+++ my-submissions/1116-maximum-level-sum-of-a-binary-tree/solution.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxLevelSum = function (root) {
+    if (!root) return 0;
+
+    let queue = [root];
+    let maxSum = -Infinity;
+    let maxLevel = 1;
+    let currentLevel = 1;
+
+    while (queue.length > 0) {
+        let levelSize = queue.length;
+        let levelSum = 0;
+
+        for (let i = 0; i < levelSize; i++) {
+            let node = queue.shift();
+            levelSum += node.val;
+
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+
+        if (levelSum > maxSum) {
+            maxSum = levelSum;
+            maxLevel = currentLevel;
+        }
+        currentLevel++;
+    }
+    return maxLevel;
+};
