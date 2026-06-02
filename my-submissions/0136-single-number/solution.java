59e740ac25a941f75d50e76303af336f85bb01a7
Amrit Singh <amrit.sbagga@gmail.com>
2026-06-02 14:31:58 +0000

    Sync LeetCode submission Runtime - 1 ms (99.96%), Memory - 46.3 MB (98.30%)

--- my-submissions/0136-single-number/solution.java
+++ my-submissions/0136-single-number/solution.java
@@ -1,18 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int res = 0;
-
-        for(int n : nums){
-            res ^= n;
+        int result = 0;
+        for (int num : nums) {
+           result ^= num;
         }
-        return res;
+        return result;
     }
 }
-
-// XOR 
-// Same bits 0 ^ 0 = 0, 1 ^ 1 = 0
-// Diff bits 0 ^ 1 = 1, 1 ^ 0 = 1
-// Same no. XOR to 0
-// a ^ a = 0
-// XOR with 0 return original number
-// a ^ 0 = a
