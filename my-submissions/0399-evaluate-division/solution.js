5d83b667db901fcfd90e0cd1829da34dfda5f704
Amrit Singh <amrit.sbagga@gmail.com>
2026-06-04 13:27:41 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 53.9 MB (51.99%)

---
+++ my-submissions/0399-evaluate-division/solution.js
@@ -0,0 +1,63 @@
+/**
+ * @param {string[][]} equations
+ * @param {number[]} values
+ * @param {string[][]} queries
+ * @return {number[]}
+ */
+var calcEquation = function (equations, values, queries) {
+    let graph = {};
+
+    for (let i = 0; i < equations.length; i++) {
+        let [numerator, denominator] = equations[i];
+
+        let value = values[i];
+
+        if (!graph[numerator]) {
+            graph[numerator] = {};
+        }
+
+        if (!graph[denominator]) {
+            graph[denominator] = {};
+        }
+
+        graph[numerator][denominator] = value;
+        graph[denominator][numerator] = 1 / value;
+    }
+
+    // DFS Traversal
+    let evaluateQuery = (numerator, denominator, visited) => {
+        if (!(numerator in graph) || !(denominator in graph)) {
+            return -1.0;
+        }
+
+        if (numerator === denominator) {
+            return 1.0;
+        }
+
+        visited.add(numerator);
+        let neighbors = graph[numerator];
+
+        for (let neighbor in neighbors) {
+
+            if (!visited.has(neighbor)) {
+
+                let result = evaluateQuery(neighbor, denominator, visited);
+
+                if (result !== -1.0) {
+                    return neighbors[neighbor] * result;
+                }
+            }
+        }
+        return -1.0;
+    }
+
+    // Process queries
+    let results = [];
+    for (let query of queries) {
+        let [numerator, denominator] = query;
+        let visited = new Set();
+        let result = evaluateQuery(numerator, denominator, visited);
+        results.push(result);
+    }
+    return results;
+};
