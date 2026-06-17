94fd76397da09b9b4f7838c0e0bc1f397c6f2085
Amrit Singh <amrit.sbagga@gmail.com>
2026-06-17 11:24:27 +0000

    Sync LeetCode submission Runtime - 164 ms (7.62%), Memory - 82.3 MB (33.64%)

---
+++ my-submissions/3931-process-string-with-special-operations-i/solution.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var processStr = function (s) {
+    let res = [];
+    let n = s.length;
+
+    for (let i = 0; i < n; i++) {
+        let ch = s[i];
+
+        if (ch === '*') {
+            if (res.length !== 0) {
+                res.pop();
+            }
+        }
+        else if (ch === '#') {
+            res.push(...res);
+        }
+        else if (ch === '%') {
+            res.reverse();
+        }
+        else if (ch >= 'a' && ch <= 'z') {
+            res.push(ch);
+        }
+    }
+
+    return res.join('');
+};
