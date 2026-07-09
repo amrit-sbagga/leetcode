2ec58fa680169d5d35432645af3adaced5523131
Amrit Singh <amrit.sbagga@gmail.com>
2026-07-09 13:32:33 +0000

    Sync LeetCode submission Runtime - 3 ms (97.09%), Memory - 163.1 MB (66.02%)

---
+++ my-submissions/3838-path-existence-queries-in-a-graph-i/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean[] pathExistenceQueries(int n, int[] nums, int maxDiff, int[][] queries) {
+        int[] dp = new int[n];
+        int cnt = 0;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] - nums[i - 1] > maxDiff) {
+                cnt++;
+            }
+            dp[i] = cnt;
+        }
+
+        boolean[] ans = new boolean[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            int u = queries[i][0];
+            int v = queries[i][1];
+            ans[i] = dp[u] == dp[v];
+        }
+        return ans;
+    }
+}
