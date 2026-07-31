f8738bd78a70c4093c6f2d56db93e012d3b35398
Amrit Singh <amrit.sbagga@gmail.com>
2026-07-31 13:45:27 +0000

    Sync LeetCode submission Runtime - 25 ms (57.10%), Memory - 62.8 MB (52.82%)

---
+++ my-submissions/3276-minimum-number-of-pushes-to-type-word-ii/solution.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} word
+ * @return {number}
+ */
+var minimumPushes = function(word) {
+    const freq = new Array(26).fill(0);
+
+    for (const ch of word) {
+        freq[ch.charCodeAt(0) - 97]++;
+    }
+
+    freq.sort((a, b) => b - a);
+
+    let ans = 0;
+
+    for (let i = 0; i < 26; i++) {
+        if (freq[i] === 0) break;
+
+        const pushes = Math.floor(i / 8) + 1;
+
+        ans += freq[i] * pushes;
+    }
+
+    return ans;
+};
