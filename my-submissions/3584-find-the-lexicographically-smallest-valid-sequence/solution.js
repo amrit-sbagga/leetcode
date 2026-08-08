2e29e836f5c8af1e4833baa16526bbf833aaf805
Amrit Singh <amrit.sbagga@gmail.com>
2026-08-08 18:09:55 +0000

    Sync LeetCode submission Runtime - 55 ms (93.69%), Memory - 100.6 MB (31.28%)

---
+++ my-submissions/3584-find-the-lexicographically-smallest-valid-sequence/solution.js
@@ -0,0 +1,45 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number[]}
+ */
+var validSequence = function(word1, word2) {
+    const n = word1.length;
+    const m = word2.length;
+
+    const last = new Array(m).fill(-1);
+
+    let i = n - 1;
+    let j = m - 1;
+
+    while (i >= 0 && j >= 0) {
+        if (word1[i] === word2[j]) {
+            last[j] = i;
+            --j;
+        }
+
+        --i;
+    }
+
+    const ans = [];
+    let canSkip = true;
+    j = 0;
+
+    for (i = 0; i < n && j < m; ++i) {
+        if (word1[i] === word2[j]) {
+            ans.push(i);
+            ++j;
+        }
+        else if (canSkip && (j === m - 1 || i < last[j + 1])) {
+            canSkip = false;
+            ans.push(i);
+            ++j;
+        }
+    }
+
+    if (j === m) {
+        return ans;
+    }
+
+    return [];
+};
