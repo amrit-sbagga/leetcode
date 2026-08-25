b18b15297ae139c3fb0d3652eca783029b42d52e
Amrit Singh <amrit.sbagga@gmail.com>
2026-08-25 07:09:50 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.3 MB (60.46%)

---
+++ my-submissions/4080-smallest-missing-multiple-of-k/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def missingMultiple(self, nums: List[int], k: int) -> int:
+        present = set(nums)
+
+        multiple = k
+
+        while multiple in present:
+            multiple += k
+
+        return multiple
+        
